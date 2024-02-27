c8e90c30df12e4e3acaeb271322c2b4058b6247b
Matias Moncada <matias.monc95@outlook.com>
2024-02-26 22:27:47 -0300

    Component Extend Styles listo

--- src/02-components-patterns/components/ProductCard.tsx
+++ src/02-components-patterns/components/ProductCard.tsx
@@ -1,13 +1,20 @@
-import { createContext } from 'react';
+import { ReactElement, createContext, CSSProperties } from 'react';
 import { useProduct } from '../hooks/useProduct'
-import { ProductCardProps, ProductContextProps } from '../interfaces/interfaces';
+import { Product, ProductContextProps } from '../interfaces/interfaces';
 import styles from '../styles/styles.module.css'
 
 
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
 
-export const ProductCard = ({ children, product }: ProductCardProps) => {
+export interface Props {
+  product: Product,
+  children?: ReactElement | ReactElement[],
+  className?: string;
+  style?: CSSProperties;
+}
+
+export const ProductCard = ({ children, product, className, style }: Props) => {
 
     const { counter, increaseBy } = useProduct();
 
@@ -17,7 +24,7 @@ export const ProductCard = ({ children, product }: ProductCardProps) => {
         increaseBy,
         product
     }}>
-        <div className={ styles.productCard }>
+        <div className={ `${styles.productCard} ${className}` } style={ style }>
 
             {/* <ProductImage img={ product.img } />
 
